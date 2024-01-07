a9393086764f9773a6dbf9d0f4710a11de6e9855
Rohitkr2510 <rohit70_soe@jnu.ac.in>
2024-01-07 16:01:56 +0530

    Deploy

---
+++ Day11_AdvREstFullApi/Backend/app.js
@@ -0,0 +1,71 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function (req, file, cb) {
+        cb(null, uuidv4())
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+
+}
+
+const MONGODB_URI = 'mongodb+srv://rohitkr25102002:Zm1bzLjLxhIev34E@cluster0.b0xkkay.mongodb.net/messages'
+
+const authRoutes = require('./routes/auth');
+const feedRoutes = require('./routes/feed');
+
+const app = express();
+
+//app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json())
+app.use(
+    multer({ storage: storage, fileFilter: fileFilter }).single('image')
+)
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes)
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data })
+})
+
+mongoose.connect(MONGODB_URI)
+    .then(result => {
+        console.log('connected to server')
+        const server = app.listen(8080);
+        const io = require('./socket').init(server)
+        io.on('connection', socket => {
+            console.log('Client connected');
+        })
+    })
+    .catch(err => console.log(err));
